b2e16a8061b5a41da67a17400a7962d40ff2f84c
RaulNar <166429034+RaulNar@users.noreply.github.com>
2024-04-28 13:33:47 +0200

    Update far_far_west_inc_.sql to finished prototype v.1
    Este es el prototipo completado. La base está configurada por Sergio Turrillo, un quinto de las tablas están configuradas por Asunción Naranjo y el último quinto están configuradas por Raúl Narváez

--- far_far_west_inc_.sql
+++ far_far_west_inc_.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-04-2024 a las 23:39:51
+-- Tiempo de generación: 28-04-2024 a las 12:10:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -27,14 +27,11 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `cliente`
 --
 
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
   `dni` varchar(9) NOT NULL,
   `nº_reservas` int(11) NOT NULL,
-  `id_pedido_fav` int(11) NOT NULL,
-  PRIMARY KEY (`id_cliente`),
-  KEY `dni` (`dni`)
+  `id_pedido_fav` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -43,18 +40,13 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 -- Estructura de tabla para la tabla `factura`
 --
 
-DROP TABLE IF EXISTS `factura`;
-CREATE TABLE IF NOT EXISTS `factura` (
-  `id_factura` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `factura` (
+  `id_factura` int(11) NOT NULL,
   `id_pedido` int(11) NOT NULL,
   `id_pago` int(11) NOT NULL,
   `id_sucursal` int(11) NOT NULL,
   `fecha_factura` date NOT NULL,
-  `monto_total` int(11) NOT NULL,
-  PRIMARY KEY (`id_factura`),
-  KEY `id_pedido` (`id_pedido`),
-  KEY `id_sucursal` (`id_sucursal`),
-  KEY `id_pago` (`id_pago`)
+  `monto_total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -63,16 +55,27 @@ CREATE TABLE IF NOT EXISTS `factura` (
 -- Estructura de tabla para la tabla `ingrediente`
 --
 
-DROP TABLE IF EXISTS `ingrediente`;
-CREATE TABLE IF NOT EXISTS `ingrediente` (
-  `id_ingrediente` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ingrediente` (
+  `id_ingrediente` int(11) NOT NULL,
   `id_plato` int(11) NOT NULL,
   `id_sucursal` int(11) NOT NULL,
   `tipo_producto` enum('carnes','pescados','lacteos','verduras','otros') NOT NULL,
-  `stock` int(11) NOT NULL,
-  PRIMARY KEY (`id_ingrediente`),
-  KEY `id_plato` (`id_plato`,`id_sucursal`),
-  KEY `id_sucursal` (`id_sucursal`)
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `linea_pedido`
+--
+
+CREATE TABLE `linea_pedido` (
+  `id_linea_pedido` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_plato` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `cantidad` int(2) UNSIGNED NOT NULL,
+  `precio_total` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -81,14 +84,26 @@ CREATE TABLE IF NOT EXISTS `ingrediente` (
 -- Estructura de tabla para la tabla `pago`
 --
 
-DROP TABLE IF EXISTS `pago`;
-CREATE TABLE IF NOT EXISTS `pago` (
-  `id_pago` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pago` (
+  `id_pago` int(11) NOT NULL,
   `id_pedido` int(11) NOT NULL,
   `nº_tarjeta` int(20) NOT NULL,
-  `fecha_pago` date NOT NULL,
-  PRIMARY KEY (`id_pago`),
-  KEY `id_pedido` (`id_pedido`)
+  `fecha_pago` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id_pedido` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `servidor` int(11) NOT NULL,
+  `direccion` varchar(30) NOT NULL,
+  `precio_pedido` float NOT NULL,
+  `fecha` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -97,15 +112,13 @@ CREATE TABLE IF NOT EXISTS `pago` (
 -- Estructura de tabla para la tabla `persona`
 --
 
-DROP TABLE IF EXISTS `persona`;
-CREATE TABLE IF NOT EXISTS `persona` (
+CREATE TABLE `persona` (
   `DNI` varchar(9) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `edad` int(11) NOT NULL,
   `direccion` varchar(50) NOT NULL,
-  `telefono` int(11) NOT NULL,
-  PRIMARY KEY (`DNI`)
+  `telefono` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -114,14 +127,12 @@ CREATE TABLE IF NOT EXISTS `persona` (
 -- Estructura de tabla para la tabla `plato`
 --
 
-DROP TABLE IF EXISTS `plato`;
-CREATE TABLE IF NOT EXISTS `plato` (
-  `id_plato` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `plato` (
+  `id_plato` int(11) NOT NULL,
   `nombre_plato` int(20) NOT NULL,
   `descripcion` varchar(50) NOT NULL,
   `precio_plato` double NOT NULL,
-  `tipo_plato` enum('Bebidas','Postres','Plato Principal','Plato Secundario') NOT NULL,
-  PRIMARY KEY (`id_plato`)
+  `tipo_plato` enum('Bebidas','Postres','Plato Principal','Plato Secundario') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -130,14 +141,11 @@ CREATE TABLE IF NOT EXISTS `plato` (
 -- Estructura de tabla para la tabla `proveedor`
 --
 
-DROP TABLE IF EXISTS `proveedor`;
-CREATE TABLE IF NOT EXISTS `proveedor` (
-  `id_proveedor` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL,
   `id_sucursal` int(11) NOT NULL,
   `nombre_completo` varchar(50) NOT NULL,
-  `telefono` int(9) NOT NULL,
-  PRIMARY KEY (`id_proveedor`),
-  KEY `id_sucursal` (`id_sucursal`)
+  `telefono` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -146,17 +154,12 @@ CREATE TABLE IF NOT EXISTS `proveedor` (
 -- Estructura de tabla para la tabla `reserva`
 --
 
-DROP TABLE IF EXISTS `reserva`;
-CREATE TABLE IF NOT EXISTS `reserva` (
-  `id_reserva` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reserva` (
+  `id_reserva` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `id_sucursal` int(11) NOT NULL,
   `nombre_reserva` varchar(30) NOT NULL,
-  `fecha_pago` date NOT NULL,
-  PRIMARY KEY (`id_reserva`),
-  KEY `id_cliente` (`id_cliente`),
-  KEY `id_sucursal` (`id_sucursal`),
-  KEY `id_cliente_2` (`id_cliente`,`id_sucursal`)
+  `fecha_pago` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -165,15 +168,13 @@ CREATE TABLE IF NOT EXISTS `reserva` (
 -- Estructura de tabla para la tabla `sucursales`
 --
 
-DROP TABLE IF EXISTS `sucursales`;
-CREATE TABLE IF NOT EXISTS `sucursales` (
-  `id_sucursal` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sucursales` (
+  `id_sucursal` int(11) NOT NULL,
   `nombre_sucursal` int(20) NOT NULL,
   `direccion` varchar(50) NOT NULL,
   `telefono` int(9) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `horario` date NOT NULL,
-  PRIMARY KEY (`id_sucursal`)
+  `horario` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -182,16 +183,12 @@ CREATE TABLE IF NOT EXISTS `sucursales` (
 -- Estructura de tabla para la tabla `trabajador`
 --
 
-DROP TABLE IF EXISTS `trabajador`;
-CREATE TABLE IF NOT EXISTS `trabajador` (
-  `id_trabjador` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `trabajador` (
+  `id_trabjador` int(11) NOT NULL,
   `dni` varchar(9) NOT NULL,
   `id_sucursal` int(11) NOT NULL,
   `salario` double NOT NULL,
-  `cargo` enum('Jefe','Chef','Camarero','Repartidor') NOT NULL,
-  PRIMARY KEY (`id_trabjador`),
-  KEY `dni` (`dni`,`id_sucursal`),
-  KEY `id_sucursal` (`id_sucursal`)
+  `cargo` enum('Jefe','Chef','Camarero','Repartidor') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -200,16 +197,179 @@ CREATE TABLE IF NOT EXISTS `trabajador` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id_factura` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `id_factura` int(10) NOT NULL,
   `dni` varchar(9) NOT NULL,
   `contraseña` char(20) NOT NULL,
   `permisos` int(10) NOT NULL,
-  `nombre_usuario` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_factura`),
-  KEY `dni` (`dni`)
+  `nombre_usuario` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id_factura`),
+  ADD KEY `id_pedido` (`id_pedido`),
+  ADD KEY `id_sucursal` (`id_sucursal`),
+  ADD KEY `id_pago` (`id_pago`);
+
+--
+-- Indices de la tabla `ingrediente`
+--
+ALTER TABLE `ingrediente`
+  ADD PRIMARY KEY (`id_ingrediente`),
+  ADD KEY `id_plato` (`id_plato`,`id_sucursal`),
+  ADD KEY `id_sucursal` (`id_sucursal`);
+
+--
+-- Indices de la tabla `linea_pedido`
+--
+ALTER TABLE `linea_pedido`
+  ADD PRIMARY KEY (`id_linea_pedido`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_plato` (`id_plato`),
+  ADD KEY `id_pedido` (`id_pedido`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`id_pago`),
+  ADD KEY `id_pedido` (`id_pedido`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `servidor` (`servidor`),
+  ADD KEY `servidor_2` (`servidor`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`DNI`);
+
+--
+-- Indices de la tabla `plato`
+--
+ALTER TABLE `plato`
+  ADD PRIMARY KEY (`id_plato`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`),
+  ADD KEY `id_sucursal` (`id_sucursal`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_sucursal` (`id_sucursal`),
+  ADD KEY `id_cliente_2` (`id_cliente`,`id_sucursal`);
+
+--
+-- Indices de la tabla `sucursales`
+--
+ALTER TABLE `sucursales`
+  ADD PRIMARY KEY (`id_sucursal`);
+
+--
+-- Indices de la tabla `trabajador`
+--
+ALTER TABLE `trabajador`
+  ADD PRIMARY KEY (`id_trabjador`),
+  ADD KEY `dni` (`dni`,`id_sucursal`),
+  ADD KEY `id_sucursal` (`id_sucursal`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_factura`),
+  ADD KEY `dni` (`dni`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `id_factura` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ingrediente`
+--
+ALTER TABLE `ingrediente`
+  MODIFY `id_ingrediente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `plato`
+--
+ALTER TABLE `plato`
+  MODIFY `id_plato` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sucursales`
+--
+ALTER TABLE `sucursales`
+  MODIFY `id_sucursal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `trabajador`
+--
+ALTER TABLE `trabajador`
+  MODIFY `id_trabjador` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_factura` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -234,6 +394,27 @@ ALTER TABLE `factura`
 ALTER TABLE `ingrediente`
   ADD CONSTRAINT `ingrediente_ibfk_1` FOREIGN KEY (`id_plato`) REFERENCES `plato` (`id_plato`),
   ADD CONSTRAINT `ingrediente_ibfk_2` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursales` (`id_sucursal`);
+
+--
+-- Filtros para la tabla `linea_pedido`
+--
+ALTER TABLE `linea_pedido`
+  ADD CONSTRAINT `lineaPedido_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lineaPedido_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lineaPedido_plato` FOREIGN KEY (`id_plato`) REFERENCES `plato` (`id_plato`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD CONSTRAINT `pago_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sercidor_trabajador` FOREIGN KEY (`servidor`) REFERENCES `trabajador` (`id_trabjador`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `proveedor`
